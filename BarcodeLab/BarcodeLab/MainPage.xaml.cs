15a05d662796edab0f0624c03d17ecb5e93eb8bd
ncarandini <n.carandini@outlook.com>
2019-01-24 23:03:32 +0100

    Add solution BarcodeLab

---
+++ BarcodeLab/BarcodeLab/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Barcode.Generator;
+using Barcode.Generator.Common;
+using Barcode.Generator.Rendering;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BarcodeLab
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void CreaBarcodeButton_Clicked(object sender, EventArgs e)
+        {
+            var barcodeWriter = new BarcodeWriterPixelData
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new EncodingOptions
+                {
+                    Width = 200,
+                    Height = 200,
+                    Margin = 0
+                }
+            };
+
+            PixelData pixelData = barcodeWriter.Write(BarcodeText.Text);
+            byte[] bmpArray = BitmapConverter.FromPixelData(pixelData);
+
+            BarcodeImage.Source = ImageSource.FromStream(() => new MemoryStream(bmpArray));
+        }
+    }
+}
